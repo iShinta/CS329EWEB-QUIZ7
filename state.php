b131bcb83e300fe9a3c5d4cff1481dc8901a38a4
iShinta <ho_minh_tri@yahoo.fr>
2016-11-11 20:13:02 -0600

    Test 0.1

--- state.php
+++ state.php
@@ -1,38 +1,4 @@
-<?php
-function start(){
-  if(isset($_COOKIE["id"])){ //In a session
-    //Recuperer les variables de la session
-    echo "Logged in a session";
-    showLogged();
-  }else{ //Is not in a session
-    if(isset($_POST)){
-      echo "Not in a session, Form Posted"
-    }else{
-      echo "Not in a session, Showing Login"
-      showLogin();
-    }
-  }
-}
 
-function showLogin(){ ?>
-  <form>
-    <input type="text" name="username" />
-    <input type="text" name="password" />
-    <input type="submit" name="submit" value="Submit" />
-    <input type="reset" name="reset" value="Reset" />
-  </form>
-<?php }
-
-function showLogged(){
-  //Form has been submitted, check if login ok
-
-}
-
-function showLoggedOut(){
-  session_unset();
-  session_destroy();
-}
-?>
 
 <!DOCTYPE html>
 <html>
@@ -40,6 +6,6 @@ function showLoggedOut(){
   </head>
 
   <body>
-    <?php start(); ?>
+    Hello wesh
   </body>
 </html>
