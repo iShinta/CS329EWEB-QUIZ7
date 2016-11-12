034798a09840a1ef4e27b9c6e7088aa9801304f2
iShinta <ho_minh_tri@yahoo.fr>
2016-11-11 20:16:02 -0600

    Test 0.2

--- state.php
+++ state.php
@@ -1,17 +1,6 @@
 <?php
 function start(){
-  if(isset($_COOKIE["id"])){ //In a session
-    //Recuperer les variables de la session
-    echo "Logged in a session";
-    //showLogged();
-  }else{ //Is not in a session
-    if(isset($_POST)){
-      echo "Not in a session, Form Posted";
-    }else{
-      echo "Not in a session, Showing Login"
-      //showLogin();
-    }
-  }
+  echo "Hello wesh";
 }
 ?>
 
