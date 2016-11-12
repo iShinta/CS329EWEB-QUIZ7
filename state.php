2d3e23ae70382bb384eb2fc1830740594bdd686c
iShinta <ho_minh_tri@yahoo.fr>
2016-11-11 20:11:00 -0600

    Messages 0.1

--- state.php
+++ state.php
@@ -2,11 +2,13 @@
 function start(){
   if(isset($_COOKIE["id"])){ //In a session
     //Recuperer les variables de la session
+    echo "Logged in a session";
     showLogged();
   }else{ //Is not in a session
     if(isset($_POST)){
-
+      echo "Not in a session, Form Posted"
     }else{
+      echo "Not in a session, Showing Login"
       showLogin();
     }
   }
