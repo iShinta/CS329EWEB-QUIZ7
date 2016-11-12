a526f7fe87be3ccdf741dd75dcc09fba423c66d4
iShinta <ho_minh_tri@yahoo.fr>
2016-11-11 20:35:54 -0600

    Test 0.8

--- state.php
+++ state.php
@@ -7,8 +7,8 @@ function start(){
   }else{ //Is not in a session
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
       echo "Not in a session, Form Posted";
-      $username = $_SERVER[$POST["id"]];
-      $password = $_SERVER[$POST["password"]];
+      $username = $POST["id"];
+      $password = $POST["password"];
 
       if($username == "guest" && $password == "welcome"){
         echo $username;
