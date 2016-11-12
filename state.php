c8ada98ac33afd7c5227463722a71a6f49b81172
iShinta <ho_minh_tri@yahoo.fr>
2016-11-11 20:26:37 -0600

    Test 0.5

--- state.php
+++ state.php
@@ -5,27 +5,33 @@ function start(){
     echo "Logged in a session";
     showLogged();
   }else{ //Is not in a session
-    if(isset($_POST)){
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
       echo "Not in a session, Form Posted"
     }else{
       echo "Not in a session, Showing Login"
-      
+      showLogin();
     }
   }
 }
-//
-// function showLogin(){
 
-//
-// function showLogged(){
-//   //Form has been submitted, check if login ok
-//
-// }
-//
-// function showLoggedOut(){
-//   session_unset();
-//   session_destroy();
-// }
+function showLogin(){ ?>
+  <form>
+    <input type="text" name="username" />
+    <input type="text" name="password" />
+    <input type="submit" name="submit" value="Submit" />
+    <input type="reset" name="reset" value="Reset" />
+  </form>
+<?php }
+
+function showLogged(){
+  //Form has been submitted, check if login ok
+
+}
+
+function showLoggedOut(){
+  session_unset();
+  session_destroy();
+}
 ?>
 
 <!DOCTYPE html>
